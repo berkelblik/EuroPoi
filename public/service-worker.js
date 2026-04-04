a690f3c2c69aacba8b5d75d137ff21f4c1489662
berkelblik <berkelblik@gmail.com>
2026-04-04 10:28:02 +0200

    Service worker en nieuwe bestanden toegevoegd

---
+++ public/service-worker.js
@@ -0,0 +1,44 @@
+const CACHE = "europoi-v3";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/EuroPoiLogo.png",
+  "/bike-bell-40094.mp3"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("message", (e) => {
+  if (e.data === "SKIP_WAITING") self.skipWaiting();
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.url.includes("tile.openstreetmap.org") ||
+      e.request.url.includes("arcgisonline.com") ||
+      e.request.url.includes("elevenlabs.io")) {
+    return;
+  }
+  e.respondWith(
+    fetch(e.request)
+      .then((resp) => {
+        const copy = resp.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy));
+        return resp;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
